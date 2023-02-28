ca45539e4879c1ffe253fb1ceea11d9aeaa25a26
Ariston <ariston.cdsf@gmail.com>
2023-02-28 16:02:51 -0300

    Habilitei a movimentação da janela

--- script.js
+++ script.js
@@ -72,3 +72,100 @@ player.addEventListener('click',()=>{
 
 });
 
+
+
+
+const janela = document.querySelector('.janela');
+
+let inicialX; // coord inicial do objeto no eixo X
+let inicialY; // coord inicial do objeto no eixo Y
+let Arrastando = false; // estado do objeto se esta arrastando ou não
+
+let atualX ; // coord atual do objeto no eixo X
+let atualY; // coord atual do objeto no eixo Y
+
+let offsetX = 0; //diferença da atual para a inicial X
+let offsetY = 0; // diferença da atual para a inicial Y
+
+
+
+
+
+
+janela.addEventListener('mousedown',Movimentar);
+document.addEventListener('mousemove',Andar);
+document.addEventListener("mouseup",Parar);
+
+
+
+function Movimentar(e){
+
+    inicialX = e.clientX - offsetX ; 
+    inicialY = e.clientY - offsetY;
+
+
+   if(e.target == janela){
+     
+    Arrastando = true;
+
+      
+   }
+
+
+
+
+    
+
+
+}
+
+function Andar(e){
+
+    if(Arrastando){
+        e.preventDefault();
+       
+        atualX = e.clientX - inicialX;
+
+        atualY = e.clientY - inicialY;
+
+        offsetX = atualX;
+
+        offsetY = atualY;
+
+        Atualizar(atualX,atualY,janela);
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
+function Parar(){
+
+    inicialX = atualX; // atualizando os valores iniciais
+    inicialY = atualY;  // atualizando as novas posiçoes atuais
+
+    Arrastando = false;
+
+
+}
+
+function Atualizar(xPos,yPos,el){
+
+    
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
+
+
+
+
+
